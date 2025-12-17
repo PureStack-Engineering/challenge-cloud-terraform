fc9256aa21b426fb4bc21ae1245cc3b380e02da8
José Luis Mora Castilla <Jlmoracastilla@gmail.com>
2025-12-17 19:58:05 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,11 @@
-variable "region" {
-  default = "us-east-1"
-}
+# -----------------------------------------------------------------------------
+# 🥈 LEVEL 2 TASK: REMOVE HARDCODING
+# -----------------------------------------------------------------------------
+# Use this file to declare variables (e.g., bucket_name, region, cidr_blocks).
+# Then, refactor main.tf to use var.variable_name instead of hardcoded strings.
+# -----------------------------------------------------------------------------
+
+# variable "aws_region" {
+#   description = "AWS Region to deploy resources"
+#   default     = "us-east-1"
+# }
